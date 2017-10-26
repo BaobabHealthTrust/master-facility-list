ad9121ca01f7838f4497e2f3915979b70a8ee8b4
liwu <jeremiahchienda@gmail.com>
2017-10-26 11:43:32 +0200

    Made the Navbar Responsive and fixed logo cropping

--- client-src/src/common/table.js
+++ client-src/src/common/table.js
@@ -1,39 +1,41 @@
-import React, { Component } from "react";
-import { Table } from "react-materialize";
+import React, { Component } from 'react';
+import { Table } from 'react-materialize';
 
 class MflTable extends Component {
-  constructor(props) {
-    super(props);
-  }
+    constructor(props) {
+        super(props);
+    }
 
-  render() {
-    const headerRows = this.props.data.headers.map(function(header) {
-      return <th>{header}</th>;
-    });
+    render() {
+        const headerRows = this.props.data.headers.map(header => {
+            return <th>{header}</th>;
+        });
 
-    const headerRecords = this.props.data.records.map(function(record) {
-      return (
-        <tr>
-          {record.map(function(cell) {
-            return <td>{cell}</td>;
-          })}
-        </tr>
-      );
-    });
-    return (
-      <div>
-        <div className="container mfl-container">
-          <Table striped className="mfl-table z-depth-2 centered">
-            <thead className="grey darken-1 white-text">
-              <tr>{headerRows}</tr>
-            </thead>
+        const headerRecords = this.props.data.records.map(record => {
+            return (
+                <tr>
+                    {record.map(cell => {
+                        return <td>{cell}</td>;
+                    })}
+                </tr>
+            );
+        });
 
-            <tbody>{headerRecords}</tbody>
-          </Table>
-        </div>
-      </div>
-    );
-  }
+        return (
+            <div>
+                <Table
+                    striped
+                    className="mfl-table z-depth-2 centered responsive-table"
+                >
+                    <thead className="grey">
+                        <tr>{headerRows}</tr>
+                    </thead>
+
+                    <tbody>{headerRecords}</tbody>
+                </Table>
+            </div>
+        );
+    }
 }
 
 export default MflTable;
