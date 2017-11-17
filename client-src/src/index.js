2ebbedd41d36cfc174cdc0593c89ece0f8765621
amusukwa <amusukwa@gmail.com>
2017-11-17 14:35:39 +0200

    cleared changes

--- client-src/src/index.js
+++ client-src/src/index.js
@@ -4,11 +4,13 @@ import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import { BrowserRouter as Router } from "react-router-dom";
-
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 ReactDOM.render(
-	<Router>
-		<App />
-	</Router>,
+	
+		<Router>
+			<App />
+		</Router>,
 	document.getElementById("root")
 );
 registerServiceWorker();
