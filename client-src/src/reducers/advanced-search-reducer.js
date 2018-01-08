2946ac8d4444761a53bc29f4daca2a5989b5f132
liwu <jeremiahchienda@gmail.com>
2018-01-08 20:03:05 +0200

    can now do a search on a combination of facility operational status and locations

--- client-src/src/reducers/advanced-search-reducer.js
+++ client-src/src/reducers/advanced-search-reducer.js
@@ -2,21 +2,33 @@ import { uniq, without, pull } from "lodash";
 
 export default (
     state = {
-        districtValues: []
+        districtValues: [],
+        operationalStatusValues: []
     },
     action
 ) => {
     switch (action.type) {
         case "ADD_DISTRICT_VALUES":
             return {
-                districtValues: uniq([...state.districtValues, action.payload])
+                districtValues: uniq([...state.districtValues, action.payload]),
+                operationalStatusValues: state.operationalStatusValues
+            };
+        case "ADD_OPERATIONAL_STATUS_VALUES":
+            const value = action.payload;
+            return {
+                districtValues: state.districtValues,
+                operationalStatusValues: uniq([
+                    ...state.operationalStatusValues,
+                    action.payload
+                ])
             };
         case "REMOVE_DISTRICT_VALUES":
             return {
                 districtValues: pull(
                     state.districtValues,
                     action.payload.toString()
-                )
+                ),
+                operationalStatusValues: state.operationalStatusValues
             };
         default:
             return state;
--- client-src/src/reducers/search-reducer.js
+++ client-src/src/reducers/search-reducer.js
@@ -1,12 +1,26 @@
-export default (state = { quickSearchFacilities: [] }, action) => {
+export default (
+    state = {
+        quickSearchFacilities: [],
+        advancedSearchFacilities: {
+            districtResultsFacilities: []
+        }
+    },
+    action
+) => {
     switch (action.type) {
-        case 'FETCH_QUICK_SEARCH':
+        case "FETCH_QUICK_SEARCH":
             return {
-                quickSearchFacilities: action.payload.data
+                quickSearchFacilities: action.payload.data,
+                advancedSearchFacilities: state.advancedSearchFacilities
+            };
+        case "FETCH_DISTRICT_RESULTS":
+            return {
+                quickSearchFacilities: state.quickSearchFacilities,
+                advancedSearchFacilities: {
+                    districtResultsFacilities: action.payload.data
+                }
             };
         default:
-            return {
-                quickSearchFacilities: state.quickSearchFacilities
-            };
+            return state;
     }
 };
