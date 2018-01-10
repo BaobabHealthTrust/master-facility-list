c59e3a18564e7046fc51d279df75a8622dae8947
lytonnyemba <lyton.m.nyemba@gmail.com>
2018-01-10 15:26:26 +0200

    removed client-src files

--- client-src/src/actions/set-current-details.js
+++
@@ -1,8 +0,0 @@
-export default function setCurrentDetails(facilities, id) {
-    const details = facilities.filter(facility => facility.id == id);
-
-    return {
-        type: "SET_CURRENT_DETAILS",
-        payload: details
-    };
-}
