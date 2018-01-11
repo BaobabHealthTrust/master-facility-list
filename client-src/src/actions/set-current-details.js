7527ee4c504f7b0b3eb62c59c85586fcbd47e999
amusukwa <amusukwa@gmail.com>
2018-01-11 10:45:07 +0200

    cleared errors in react

--- client-src/src/actions/set-current-details.js
+++ client-src/src/actions/set-current-details.js
@@ -1,5 +1,5 @@
 export default function setCurrentDetails(facilities, id) {
-    let details = facilities.filter(facility => facility.id == id);
+    let details = facilities.filter(facility => facility.id === id);
     details = details.length > 0 ? details : null;
     return {
         type: 'SET_CURRENT_DETAILS',
