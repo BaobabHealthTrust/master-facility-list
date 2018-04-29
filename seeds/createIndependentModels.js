f369ae8136bfac06602c739f9a3383e9ab44284b
Bwighane Clive Mwalwanda <bsmwalwanda@gmail.com>
2018-04-29 08:06:32 +0200

    updated seeders

---
+++ seeds/createIndependentModels.js
@@ -0,0 +1,29 @@
+"use strict";
+const dependentFactory = require('./dependentModelFactory');
+const data = require('./data');
+const factory = require('./independentModelFactory');
+const server = require("../server/server");
+const dataSource = server.dataSources.db;
+
+const createIndependentModels = async () => {
+    // await factory(server.models.Owner, data.owners);
+    // await factory(server.models.FacilityType, data.facilityTypes);
+    // await factory(server.models.RegulatoryStatus, data.regulatoryStatuses);
+    // await factory(server.models.OperationalStatus, data.operationalStatuses);
+    // await factory(server.models.Zone, data.zoneData);
+    // await factory(server.models.Client, data.users);
+    // await factory(server.models.Role, data.roles);
+    // await factory(server.models.ResourceType, data.resourceTypes);
+    // await factory(server.models.UtilityType, data.utilityTypes);
+    // await factory(server.models.ServiceType, data.serviceTypes);
+    // await dependentFactory(server.models.Zone, server.models.District, data.districts);
+
+    // await dependentFactory(server.models.ResourceType , server.models.Resource, data.resources);
+    // await dependentFactory(server.models.UtilityType , server.models.Utility, data.utilities);
+    // await dependentFactory(server.models.ServiceType , server.models.Service, data.services);
+    // await dependentFactory(server.models.ServiceType, server.models.Service, data.services);
+
+    await dataSource.disconnect();
+}
+
+createIndependentModels();
