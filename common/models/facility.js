8994018af108a3ae71d99f8a19970c8f492a3f77
mmalumbo <mmalumbo@gmail.com>
2018-06-06 10:00:36 +0200

    added a download callback

--- common/models/facility.js
+++ common/models/facility.js
@@ -1,7 +1,20 @@
 "use strict";
+
 const server = require("../../server/server");
+const generateFacilityPdfFile = require("../../download_modules/pdf-one-facility-formatter");
+
+const prepareData = (data) =>{
+    if (data.isArray){
+        return data.map(data => {
+            data.toJSON();
+        });
+    }
+    return data.toJSON();
+}
 
 module.exports = (Facility) => {
+
+
     // TODO: Re engineer download and downloadOne remote methods
     Facility.contactDetails = async (data, id, cb) => {
         await server.models.Address.create({
@@ -42,5 +55,65 @@ module.exports = (Facility) => {
             { arg: 'id', type: 'number' }
         ],
         returns: { arg: 'response', type: 'string' }
-    })
+    });
+
+    /**
+     *
+     * Generate a pdf file
+     *
+     * @param {Facility ID} id
+     * @param {Loop back callback function} cb
+     *
+     */
+    Facility.download =  async (id,cb) => {
+
+        const facility = await Facility.findOne({
+            where: {id: id},
+            include: [
+                "locations",
+                "contactPeople",
+                "regulatoryStatus",
+                "operationalStatus",
+                "owner",
+                "facilityType",
+                "addresses",
+                {district: "zone"},
+            ],
+            limit: 1
+        });
+
+        const facilityUtilities = await server.models.FacilityUtility.find({
+            where: {facility_id: id},
+            include: {utility: "utilityType"}
+        });
+
+        const facilityResources = await server.models.FacilityResource.find({
+            where: {facility_id: id},
+            include: { resource: "resourceType" }
+        });
+
+        const facilityServices = await server.models.FacilityService.find({
+            where: {facility_id: id},
+            include: { service: ["serviceType", "category"] }
+        });
+
+        await generateFacilityPdfFile(
+            facility,
+            facilityUtilities,
+            facilityResources,
+            facilityServices
+        );
+
+        return "Malu working on it.";
+    }
+
+    /** Register a remote method */
+    Facility.remoteMethod('download',{
+        description: "Download facility details",
+        accepts: { arg: 'id', type: 'number', required: true},
+        http: {path: '/download/:id', verb: 'get'},
+        returns: { arg: 'file', type: 'string' }
+    });
+
+    //"blob"\
 };
