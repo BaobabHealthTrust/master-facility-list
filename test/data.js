1fd0b63a5275aa9b340dcbb225ef062f9acf1f9b
mmalumbo <mmalumbo@gmail.com>
2018-06-25 16:30:20 +0200

    added archive client remote method and an access hook on clients query.

--- test/data.js
+++ test/data.js
@@ -1,14 +1,104 @@
+'use strict'
+
+/** Database tables */
+module.exports.tables = [
+    'ACL',
+    'AccessToken',
+    'Address',
+    'Client',
+    'ContactPeople',
+    'District',
+    'Facility',
+    'FacilityResource',
+    'FacilityService',
+    'FacilityType',
+    'FacilityUtility',
+    'Feedback',
+    'FeedbackType',
+    'Geolocation',
+    'Location',
+    'OperationalStatus',
+    'Owner',
+    'RegulatoryStatus',
+    'Resource',
+    'ResourceType',
+    'Role',
+    'RoleMapping',
+    'Service',
+    'ServiceType',
+    'User',
+    'Utility',
+    'UtilityType',
+    'Zone'
+];
+
+module.exports.zone = {zone_name: 'North Zone'};
+
+module.exports.district = {
+    district_name: "Nkhata-Bay",
+    district_code: "NB"
+};
+
+module.exports.owner = {
+    "facility_owner": "NGO",
+    "description": "Owner description"
+};
+
+module.exports.operationalStatus = {
+    facility_operational_status: "Functional",
+    description: "This facility is now operational"
+};
+
+module.exports.regulatoryStatus = {
+    facility_regulatory_status: "Registered",
+    description: "This is a registered facility"
+};
+
+module.exports.facilityType = {
+    facility_type: "District hospital",
+    description: "These facilities belongs to districts"
+};
+
+module.exports.utilityType = {
+    utility_type: 'Energy Provider',
+    description: "This is energy source"
+};
+
+module.exports.resourceType = {
+    resource_type: 'Transport',
+    description: "This is a resource type has many sub elements"
+};
+
+const serviceType = {
+    service_type: 'Clinical Services',
+    description: "These are clinical services"
+};
+
+module.exports.user = {
+  username: "mfladminuser",
+  password: "admin",
+  firstname: "CMED",
+  lastname: "Malawi",
+  email: "administrator@gmail.com"
+};
+
 module.exports.facility = {
     facility_code: "NB01042",
     facility_name: "Nkhata-Bay Clinic",
     common_name: "Jonilenge",
-    facility_date_opened: "2017-10-25T13:27:53.703Z",
+    facility_date_opened: "2016-10-25T13:27:53.703Z",
     facility_type_id: 1,
     facility_owner_id: 1,
     facility_operational_status_id: 1,
     facility_regulatory_status_id: 1,
+    published_date: "2017-10-25T13:27:53.703Z",
     district_id: 1,
     client_id: 1
+};
+
+module.exports.location = {
+  catchment_area: "Area 3",
+  catchment_population: 5000
 };
 
 module.exports.contactPeople = {
@@ -48,7 +138,3 @@ module.exports.service = {
     "service_category_id": 0,
 };
 
-module.exports.user = {
-    password: "haxy",
-    email: "haroon@gmail.com"
-}
