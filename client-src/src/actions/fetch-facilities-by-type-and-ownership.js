6bfbdc032e74cd37ec3478ebc4dad5d757ca82ba
mmalumbo <mmalumbo@gmail.com>
2018-07-05 14:37:54 +0200

    wired up facilities with types and owners  graph.

--- client-src/src/actions/fetch-facilities-by-type-and-ownership.js
+++ client-src/src/actions/fetch-facilities-by-type-and-ownership.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import settings from '../settings';
 
-export default operationalStatuses => {
-    const END_POINT = `${settings.hostname}/api/Facilities/aggregates/typeandownership`;
+export default districts => {
+    const END_POINT = `${settings.hostname}/api/Facilities/aggregates/typeandownership?districts=${JSON.stringify(districts)}`;
     const request = axios.get(END_POINT);
     return {
         type: "FETCH_FACILITIES_BY_TYPE_AND_OWNERSHIP",
