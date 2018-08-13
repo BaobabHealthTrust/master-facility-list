4c25bde8cf6452a742d2a17f37bc66a1a57bf376
ibwighane <bsmwalwanda@gmail.com>
2018-08-13 14:01:20 +0200

    created first iteration on fhir endpoints

--- client-src/src/settings.js
+++ client-src/src/settings.js
@@ -1,4 +1,3 @@
 export default {
-    hostname: "http://localhost:3000",
-    accessToken: "i6I5Lt5jkrGfwmI7ls7TlWUmDzTds0UGF9fwBaR1smPgLTBjBddtBi82fyv6BiW"
+    hostname: "http://127.0.0.1:3000"
 };
