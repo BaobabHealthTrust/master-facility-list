44f1d09beb430d5f1c26158e5cf88a8083719bde
liwu <jeremiahchienda@gmail.com>
2018-08-24 10:37:22 +0200

    cleaned up onclick logic

--- client-src/src/Dashboard/charts/FacilitiesMap.js
+++ client-src/src/Dashboard/charts/FacilitiesMap.js
@@ -5,9 +5,10 @@ class FacilitiesMap extends Component {
 
   isHiglighted = (district) => this.props.districts.includes(district);
 
+
   render() {
     const view = (
-      <svg id='svg' className='map' viewBox="0 0 195 481" version="1.1" style={{ height: 600 }}>
+      <svg id='svg' className='map' viewBox="0 0 195 481" version="1.1" style={{ height: this.props.height - 10 }}>
         <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
           <g id="Dashboard" transform="translate(-68.000000, -231.000000)" fill="#9D9D9D">
             <g id="Map-Filter" transform="translate(26.000000, 160.000000)">
