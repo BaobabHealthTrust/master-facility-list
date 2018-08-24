9315a2947d9dc35a223961fc6caa7651b7a4745d
liwu <jeremiahchienda@gmail.com>
2018-08-24 09:51:55 +0200

    map made sticky

---
+++ client-src/src/images/index.js
@@ -0,0 +1,6 @@
+//@flow
+export { default as bloodTest } from "./blood-test.svg";
+export { default as hospital } from "./hospital.svg";
+export { default as patient } from "./patient.svg";
+export { default as pregnant } from "./pregnant.svg";
+export { default as ribbon } from "./ribbon.svg";
