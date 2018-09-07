a247760b94cfa225a58c4d764de49e433fd306a1
liwu <jeremiahchienda@gmail.com>
2018-09-07 11:00:49 +0200

    fixed issues with closing tags

--- client-src/src/Facility/AdvancedSearch/OperationalStatusTags.js
+++ client-src/src/Facility/AdvancedSearch/OperationalStatusTags.js
@@ -3,37 +3,37 @@ import { connect } from "react-redux";
 import SearchTag from "./SearchTag";
 import { fetchBasicDetailsResults, removeSearchValues } from "../../actions";
 class OperationalStatusTags extends Component {
-    render() {
-        return this.props
-            .getObjectFromIds(
-            this.props.searchValues.operationalStatusValues,
-            this.props.operationalStatuses
-            )
-            .map(entity => {
-                return (
-                    <SearchTag
-                        name={entity.facility_operational_status}
-                        id={entity.id}
-                        actionType={"REMOVE_OPERATIONAL_STATUS_VALUES"}
-                        removeSearchValues={async (id, actionType) => {
-                            await this.props.removeSearchValues(id, actionType);
-                            await this.props.fetchBasicDetailsResults(
-                                this.props.searchValues
-                            );
-                        }}
-                    />
-                );
-            });
-    }
+  render() {
+    return this.props
+      .getObjectFromIds(
+        this.props.searchValues.operationalStatusValues,
+        this.props.operationalStatuses
+      )
+      .map(entity => {
+        return (
+          <SearchTag
+            name={entity.facility_operational_status}
+            id={entity.id}
+            actionType="REMOVE_OPERATIONAL_STATUS_VALUES"
+            removeSearchValues={async (id, actionType) => {
+              await this.props.removeSearchValues(id, actionType);
+              await this.props.fetchBasicDetailsResults(
+                this.props.searchValues
+              );
+            }}
+          />
+        );
+      });
+  }
 }
 const mapStateToProps = state => {
-    return {
-        searchValues: state.advancedSearchValues,
-        operationalStatuses: state.dependancies.operationalStatuses
-    };
+  return {
+    searchValues: state.advancedSearchValues,
+    operationalStatuses: state.dependancies.operationalStatuses
+  };
 };
 
 export default connect(mapStateToProps, {
-    removeSearchValues,
-    fetchBasicDetailsResults
+  removeSearchValues,
+  fetchBasicDetailsResults
 })(OperationalStatusTags);
