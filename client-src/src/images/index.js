b5405a91ad419f4cf32a4269e0bc539c0942915d
liwu <jeremiahchienda@gmail.com>
2018-09-14 12:08:32 +0200

    changed card icons as well as adding the graph for ownership

--- client-src/src/images/index.js
+++ client-src/src/images/index.js
@@ -5,3 +5,8 @@ export { default as patient } from "./patient.svg";
 export { default as pregnant } from "./pregnant.svg";
 export { default as ribbon } from "./ribbon.svg";
 export { default as maternity } from "./maternity.svg";
+export { default as district } from "./district.svg";
+export { default as clinic } from "./clinic.svg";
+export { default as tent } from "./tent.svg";
+export { default as kids } from "./kids-smiling.jpg";
+export { default as normal_hospital } from "./normal-hospital.svg";
