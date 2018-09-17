d5539191efc9bf26680007e8002d6240cb45cf2c
liwu <jeremiahchienda@gmail.com>
2018-09-17 18:00:16 +0200

    styled feedback page

---
+++ client-src/src/common/Banner.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import styled, { css } from 'styled-components';
+import { kids, baobab, kuunika, lin, moh } from '../images'
+
+const BannerContainer = styled.div`
+  background: rgba(0,0,0,.6);
+  background-image: url(${kids});
+  color: white;
+  background-blend-mode: darken;
+  padding: 6rem 2rem;
+`
+
+const Title = styled.div.attrs({ className: "text-center text-white text-4xl" })`
+  text-shadow: 1px 0px #666;
+`
+
+const Banner = ({ title }) => {
+  return (
+    <BannerContainer>
+      <Title>{title}</Title>
+    </BannerContainer>
+  )
+}
+
+export default Banner
