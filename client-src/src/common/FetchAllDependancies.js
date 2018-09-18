03c65ac7f76d495bb1371253200bb22c00d4b709
liwu <jeremiahchienda@gmail.com>
2018-09-18 15:30:17 +0200

    pushed all dashboard logic to memory

--- client-src/src/common/FetchAllDependancies.js
+++ client-src/src/common/FetchAllDependancies.js
@@ -1,53 +1,53 @@
 //@flow
 import React from 'react'
 import {
-    fetchDistricts,
-    fetchFacilityTypes,
-    fetchFacilityOwners,
-    fetchUtilityTypes,
-    fetchResourceTypes,
-    fetchServiceTypes,
-    fetchRegulatoryStatuses,
-    fetchOperationalStatuses,
-    fetchFacilities,
-    fetchResources,
-    fetchUtilities,
-    fetchServices
+  fetchDistricts,
+  fetchFacilityTypes,
+  fetchFacilityOwners,
+  fetchUtilityTypes,
+  fetchResourceTypes,
+  fetchServiceTypes,
+  fetchRegulatoryStatuses,
+  fetchOperationalStatuses,
+  fetchFacilities,
+  fetchResources,
+  fetchUtilities,
+  fetchServices
 } from "../actions";
 import { connect } from 'react-redux'
 
 class FetchAllDependancies extends React.Component<{}> {
-    componentDidMount() {
-        this.props.fetchDistricts();
-        this.props.fetchOperationalStatuses();
-        this.props.fetchFacilityTypes();
-        this.props.fetchFacilityOwners();
-        this.props.fetchRegulatoryStatuses();
-        this.props.fetchResourceTypes();
-        this.props.fetchUtilityTypes();
-        this.props.fetchServiceTypes();
-        this.props.fetchResources();
-        this.props.fetchUtilities();
-        this.props.fetchServices();
-        this.props.fetchFacilities(1);
-    }
+  componentDidMount() {
+    this.props.fetchDistricts();
+    this.props.fetchOperationalStatuses();
+    this.props.fetchFacilityTypes();
+    this.props.fetchFacilityOwners();
+    this.props.fetchRegulatoryStatuses();
+    this.props.fetchResourceTypes();
+    this.props.fetchUtilityTypes();
+    this.props.fetchServiceTypes();
+    this.props.fetchResources();
+    this.props.fetchUtilities();
+    this.props.fetchServices();
+    this.props.fetchFacilities();
+  }
 
-    render() {
-        return <div />
-    }
+  render() {
+    return <div />
+  }
 }
 
-export default connect(state => {}, {
-    fetchDistricts,
-    fetchFacilityTypes,
-    fetchFacilityOwners,
-    fetchUtilityTypes,
-    fetchResourceTypes,
-    fetchServiceTypes,
-    fetchRegulatoryStatuses,
-    fetchOperationalStatuses,
-    fetchFacilities,
-    fetchResources,
-    fetchUtilities,
-    fetchServices
+export default connect(state => { }, {
+  fetchDistricts,
+  fetchFacilityTypes,
+  fetchFacilityOwners,
+  fetchUtilityTypes,
+  fetchResourceTypes,
+  fetchServiceTypes,
+  fetchRegulatoryStatuses,
+  fetchOperationalStatuses,
+  fetchFacilities,
+  fetchResources,
+  fetchUtilities,
+  fetchServices
 })(FetchAllDependancies)
