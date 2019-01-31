e365bb44a867db448bed715dc045955013b8ea96
McDonald Kapsata <caspater@McDonalds-MacBook-Pro.local>
2019-01-31 08:58:56 +0200

    Refactored Common Components

--- client-src/src/common/DatePicker.js
+++ client-src/src/common/DatePicker.js
@@ -1,38 +1,45 @@
 //@flow
-import React from 'react'
-import { Input } from 'react-materialize'
+import React, { Fragment } from "react";
+import { Input } from "react-materialize";
 
-export default class DatePicker extends React.Component<{}>{
+export default class DatePicker extends React.Component<{}> {
   state = {
     year: this.props.year || "1975",
     day: this.props.day || "01",
     month: this.props.month || "01"
-  }
+  };
+
+  getSelectedDate = () =>
+    `${this.state.year}-${this.state.month}-${this.state.day}`;
 
   handleChange = async (e, property: string) => {
-    await this.setState({ [property]: e.target.value })
-    await this.props.onChange(`${this.state.year}-${this.state.month}-${this.state.day}`)
-  }
+    await this.setState({ [property]: e.target.value });
+    await this.props.onChange(this.getSelectedDate());
+  };
 
   render() {
-    const { suffix } = this.props
+    const { suffix } = this.props;
+    const { year, month, day } = this.state;
     return (
-      <React.Fragment>
+      <Fragment>
         <Input
           s={2}
-          type='select'
+          type="select"
           label={`Select Year ${suffix}`}
-          onChange={(e) => this.handleChange(e, "year")}
-          value={this.state.year}
+          onChange={e => this.handleChange(e, "year")}
+          value={year}
         >
-          {[...Array(45).keys()].map(year => <option>{year + 1975}</option>)}
+          {[...Array(45).keys()].map(yr => (
+            <option key={yr}>{yr + 1975}</option>
+          ))}
         </Input>
+
         <Input
           s={2}
-          type='select'
+          type="select"
           label={`Select Month ${suffix}`}
-          onChange={(e) => this.handleChange(e, "month")}
-          value={this.state.month}
+          onChange={e => this.handleChange(e, "month")}
+          value={month}
         >
           <option value="01">Jan</option>
           <option value="02">Feb</option>
@@ -49,14 +56,16 @@ export default class DatePicker extends React.Component<{}>{
         </Input>
         <Input
           s={2}
-          type='select'
+          type="select"
           label={`Select Day ${suffix}`}
-          onChange={(e) => this.handleChange(e, "day")}
-          value={this.state.day}
+          onChange={e => this.handleChange(e, "day")}
+          value={day}
         >
-          {[...Array(31).keys()].map(day => <option>{(day + 1).toString().padStart(2, '0')}</option>)}
+          {[...Array(31).keys()].map(dy => (
+            <option>{(dy + 1).toString().padStart(2, "0")}</option>
+          ))}
         </Input>
-      </React.Fragment >
-    )
+      </Fragment>
+    );
   }
 }
