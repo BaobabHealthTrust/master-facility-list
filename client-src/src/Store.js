87d3c06c62ee976a4d3fa95f7004dad7cb809704
kapsata <mkapsata@gmail.com>
2019-03-08 09:24:41 +0200

    Added settings for debagging Redux

--- client-src/src/Store.js
+++ client-src/src/Store.js
@@ -1,7 +1,13 @@
-import {createStore, applyMiddleware} from 'redux';
-import rootReducer from './reducers/index';
-import promise from 'redux-promise';
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "./reducers/index";
+import promise from "redux-promise";
 
-const Store = createStore(rootReducer, applyMiddleware(promise));
+const Store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(promise)
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
 
 export default Store;
