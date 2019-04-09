51221534775143bea324b2f1e66280066a5d246a
kapsata <mkapsata@gmail.com>
2019-04-09 09:44:18 +0200

    Updated contacts and facility models(backend)

--- common/models/facility.js
+++ common/models/facility.js
@@ -100,38 +100,38 @@ module.exports = Facility => {
     return "Data Successfully Created";
   };
 
-  Facility.contactDetails = async (data, id, cb) => {
-    await server.models.Address.create({
-      physical_address: data.physicalAddress,
-      postal_address: data.postalAddress,
-      client_id: data.client,
-      facility_id: id
-    }).catch(err => cb(err));
+  // Facility.contactDetails = async (data, id, cb) => {
+  //   await server.models.Address.create({
+  //     physical_address: data.physicalAddress,
+  //     postal_address: data.postalAddress,
+  //     client_id: data.client,
+  //     facility_id: id
+  //   }).catch(err => cb(err));
 
-    await server.models.ContactPeople.create({
-      contact_person_fullname: data.contactName,
-      contact_person_phone: data.contactPhoneNumber,
-      contact_person_email: data.contactEmail,
-      client_id: data.client,
-      facility_id: id
-    }).catch(err => cb(err));
+  //   await server.models.ContactPeople.create({
+  //     contact_person_fullname: data.contactName,
+  //     contact_person_phone: data.contactPhoneNumber,
+  //     contact_person_email: data.contactEmail,
+  //     client_id: data.client,
+  //     facility_id: id
+  //   }).catch(err => cb(err));
 
-    await server.models.Location.create({
-      catchment_area: data.catchmentArea,
-      catchment_population: data.catchmentPopulation,
-      client_id: data.client,
-      facility_id: id
-    }).catch(err => cb(err));
+  //   await server.models.Location.create({
+  //     catchment_area: data.catchmentArea,
+  //     catchment_population: data.catchmentPopulation,
+  //     client_id: data.client,
+  //     facility_id: id
+  //   }).catch(err => cb(err));
 
-    await server.models.Geolocation.create({
-      longitude: data.longitude,
-      latitude: data.latitude,
-      client_id: data.client,
-      facility_id: id
-    }).catch(err => cb(err));
+  //   await server.models.Geolocation.create({
+  //     longitude: data.longitude,
+  //     latitude: data.latitude,
+  //     client_id: data.client,
+  //     facility_id: id
+  //   }).catch(err => cb(err));
 
-    return "Data Successfully Created";
-  };
+  //   return "Data Successfully Created";
+  // };
 
   Facility.updateContactDetails = async (data, id, cb) => {
     await server.models.Address.findOne({ where: { facility_id: id } }).then(
