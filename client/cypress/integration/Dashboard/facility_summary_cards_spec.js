c41dc0c953420f0c662ec164caf61fad8bfed2a8
kapsata <mkapsata@gmail.com>
2020-02-27 15:08:47 +0200

    updated stat cards tests

--- client/cypress/integration/Dashboard/facility_summary_cards_spec.js
+++ client/cypress/integration/Dashboard/facility_summary_cards_spec.js
@@ -14,43 +14,70 @@ describe("Test Dashboard Summary Statistics", () => {
     });
   });
 
-  it("Should show valid District Hospitals statistics", () => {
-    cy.fetch_facilieties("facility_type_id", 5).then(res => {
-      cy.get("[data-test=DistHospitals]").should("be.visible");
-      cy.get("[data-test=DistHospitals]")
-        .find("p")
-        .first()
-        .should("have.html", `${res.length}`);
+  describe("Shows valid stats", () => {
+    const FacilityTypesDisplay = {
+      Hospitals: "District Hospital",
+      HealthCenters: "Health Centre",
+      HealthPosts: "Health Post",
+      Dispensaries: "Dispensary"
+    };
+    let FacilityTypes = [];
+    it("Should get facility types", () => {
+      cy.fetch_facility_types().then(res => {
+        FacilityTypes = res;
+      });
     });
-  });
 
-  it("Should show valid Health Centers statistics", () => {
-    cy.fetch_facilieties("facility_type_id", 1).then(res => {
-      cy.get("[data-test=HealthCenters]").should("be.visible");
-      cy.get("[data-test=HealthCenters]")
-        .find("p")
-        .first()
-        .should("have.html", `${res.length}`);
+    it("Should show valid District Hospitals statistics", () => {
+      const id = FacilityTypes.find(
+        type => type.facility_type == FacilityTypesDisplay.Hospitals
+      ).id;
+      cy.fetch_facilieties("facility_type_id", id).then(res => {
+        cy.get("[data-test=Hospitals]").should("be.visible");
+        cy.get("[data-test=Hospitals]")
+          .find("p")
+          .first()
+          .should("have.html", `${res.length}`);
+      });
     });
-  });
 
-  it("Should show valid Dispensary statistics", () => {
-    cy.fetch_facilieties("facility_type_id", 2).then(res => {
-      cy.get("[data-test=Dispensaries]").should("be.visible");
-      cy.get("[data-test=Dispensaries]")
-        .find("p")
-        .first()
-        .should("have.html", `${res.length}`);
+    it("Should show valid Health Centers statistics", () => {
+      const id = FacilityTypes.find(
+        type => type.facility_type == FacilityTypesDisplay.HealthCenters
+      ).id;
+      cy.fetch_facilieties("facility_type_id", id).then(res => {
+        cy.get("[data-test=HealthCenters]").should("be.visible");
+        cy.get("[data-test=HealthCenters]")
+          .find("p")
+          .first()
+          .should("have.html", `${res.length}`);
+      });
     });
-  });
 
-  it("Should show valid Health Post statistics", () => {
-    cy.fetch_facilieties("facility_type_id", 4).then(res => {
-      cy.get("[data-test=HealthPosts]").should("be.visible");
-      cy.get("[data-test=HealthPosts]")
-        .find("p")
-        .first()
-        .should("have.html", `${res.length}`);
+    it("Should show valid Dispensary statistics", () => {
+      const id = FacilityTypes.find(
+        type => type.facility_type == FacilityTypesDisplay.Dispensaries
+      ).id;
+      cy.fetch_facilieties("facility_type_id", id).then(res => {
+        cy.get("[data-test=Dispensaries]").should("be.visible");
+        cy.get("[data-test=Dispensaries]")
+          .find("p")
+          .first()
+          .should("have.html", `${res.length}`);
+      });
+    });
+
+    it("Should show valid Health Post statistics", () => {
+      const id = FacilityTypes.find(
+        type => type.facility_type == FacilityTypesDisplay.HealthPosts
+      ).id;
+      cy.fetch_facilieties("facility_type_id", id).then(res => {
+        cy.get("[data-test=HealthPosts]").should("be.visible");
+        cy.get("[data-test=HealthPosts]")
+          .find("p")
+          .first()
+          .should("have.html", `${res.length}`);
+      });
     });
   });
 });
