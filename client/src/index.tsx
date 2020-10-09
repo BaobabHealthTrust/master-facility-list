e4fa2d866986ad42ae5847b33a924b490bf67868
kapsata <mkapsata@gmail.com>
2020-10-09 09:27:53 +0200

    GA

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -7,8 +7,9 @@ import { Provider } from "react-redux";
 import ReactGA from "react-ga";
 import Store from "./services/redux/store";
 import { createBrowserHistory } from "history";
+
 const history: any = createBrowserHistory();
-const trackingId = "UA-128959156-2"; // Replace with your Google Analytics tracking ID
+const trackingId = "UA-128959156-4"; // Replace with your Google Analytics tracking ID
 ReactGA.initialize(trackingId, { gaOptions: { siteSpeedSampleRate: 100 } });
 
 // Initialize google analytics page view tracking
