6d8cf0d2f93d6c254b5b14b0bf9117c8105eaed4
kapsata <mkapsata@gmail.com>
2020-10-28 08:32:42 +0200

    google analytic

--- client/src/App/App.tsx
+++ client/src/App/App.tsx
@@ -44,7 +44,7 @@ import { isAdmin } from "../services/helpers";
 
 const history: any = createBrowserHistory();
 
-const trackingId = "UA-128959156-4"; // Replace with your Google Analytics tracking ID
+const trackingId = "UA-128959156-2"; // Replace with your Google Analytics tracking ID
 ReactGA.initialize(trackingId, { gaOptions: { siteSpeedSampleRate: 100 } });
 
 // Initialize google analytics page view tracking
