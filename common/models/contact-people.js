3a77e60879b2ee5b234a513493e87c8675ad7d80
Daniel Mwakanema <danielmwakanema95@gmail.com>
2020-11-03 15:05:00 +0200

    refactor(ContactPeople): add lifecyle hooks

--- common/models/contact-people.js
+++ common/models/contact-people.js
@@ -1,19 +1,13 @@
 "use strict";
-const server = require("../../server/server");
+
+const { markFacilityAsUpdated } = require('../utils');
 
 module.exports = function(Contactpeople) {
-  Contactpeople.observe("after save", async function(ctx) {
-    if (ctx.instance) {
-      // console.log(ctx.instance);
-      // server.models.Facility.findById(ctx.instance.facility_id, function() {
-      //   console.log(arguments);
-      // });
-      // const facility = await server.models.Facility.findById(
-      //   ctx.instance.facility_id
-      // );
-      // console.log(facility);
-      // facility.updated_at = Date.now();
-      // await facility.save();
-    }
+  Contactpeople.observe('after update', async function (ctx) {
+    if (ctx.instance) await markFacilityAsUpdated(ctx.instance.facility_id);
+  });
+
+  Contactpeople.observe('after save', async function (ctx) {
+    if (ctx.instance) await markFacilityAsUpdated(ctx.instance.facility_id);
   });
 };
